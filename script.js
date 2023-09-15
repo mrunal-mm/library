90d2a7e4a1b83977ba5b942c9faef551c5807fa2
mrunal-mm <mrunalmm2@gmail.com>
2023-09-15 15:58:56 +0530

    Create modal with show/hide functionality The modal contain form for adding new books to library. Modal will appear on 'Add Book' button click and will disappear on 'close' svg click.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// const myLibrary = [];
+function openModal(){
+    mainSection.classList.add('main-before');
+    navbar.classList.add('nav-before');
+    modal.style.display = "flex";
+}
 
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
+function closeModal(){
+    mainSection.classList.remove('main-before');
+    navbar.classList.remove('nav-before');
+    modal.style.display = "none";
+}
 
-// function addBookToLibrary() {
-//   const book = Book();
-//   myLibrary.push(book);
-// }
+const modal = document.querySelector('.modal');
+const mainSection = document.querySelector('main');
+const navbar = document.querySelector('nav');
+
+const addBookButton = document.querySelector('#add-book')
+addBookButton.addEventListener('click', openModal);
+
+const closeModalButton = document.querySelector('#close');
+closeModalButton.addEventListener('click', closeModal);
